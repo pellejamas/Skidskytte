b22917fe3ec4a045398a55ef1eb2e77ab1ec3ebe
Pelle Andersson <pellejamas@gmail.com>
2021-03-14 21:48:08 +0100

    First commit Uploading all files

---
+++ Skidskytte/src/skidskytte/TestSkidskytte.java
@@ -0,0 +1,14 @@
+package skidskytte;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestSkidskytte {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
