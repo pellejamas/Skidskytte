7de045b535167cc082a86cacff8d3969f70576d7
Pelle Andersson <pellejamas@gmail.com>
2021-03-14 22:38:16 +0100

    My Commit I put in the 4 different methods sprint, jaktstart, masstart, distans.

--- Skidskytte/src/skidskytte/TestSkidskytte.java
+++ Skidskytte/src/skidskytte/TestSkidskytte.java
@@ -7,8 +7,54 @@ import org.junit.jupiter.api.Test;
 class TestSkidskytte {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void test01Sprint() {
+		//Arrange
+		int input = 1;
+		
+		//Act
+		Skidskytte sprint = new Skidskytte();
+		int output = sprint.sprint(input);
+		
+		//Assert
+		assertEquals(1,output);	
+	}
+	@Test
+	public void test02Jaktstart() {
+		//Arrange
+		int input = 2;
+		
+		//Act
+		Skidskytte Jaktstart = new Skidskytte();
+		int output = Jaktstart.jaktstart(input);
+		
+		//Assert
+		assertEquals(2,output);	
+	}
+	@Test
+	public void test03Distans() {
+		//Arrange
+		int input = 3;
+		
+		//Act
+		Skidskytte Distans = new Skidskytte();
+		int output = Distans.distans(input);
+		
+		//Assert
+		assertEquals(3,output);	
+	}
+	
+	@Test
+	public void test04Massstart() {
+		//Arrange
+		int input = 4;
+		
+		//Act
+		Skidskytte Masstart = new Skidskytte();
+		int output = Masstart.masstart(input);
+		
+		//Assert
+		assertEquals(4,output);	
 	}
 
+	
 }
