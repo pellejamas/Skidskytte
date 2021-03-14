45ce7745d68d6b0cd3ff59187b3677e4435c9281
Pelle Andersson <pellejamas@gmail.com>
2021-03-14 22:42:00 +0100

    Commit Message:
    I create a new class with the 4 methods from the testfile

---
+++ Skidskytte/src/skidskytte/Skidskytte.java
@@ -0,0 +1,25 @@
+package skidskytte;
+
+public class Skidskytte {
+
+	public int sprint(int input) {
+		int s = input;
+		return s;
+	}
+
+	public int jaktstart(int input) {
+		int j = input;
+		return j;
+	}
+
+	public int masstart(int input) {
+		int m = input;
+		return m;
+	}
+
+	public int distans(int input) {
+		int d = input;
+		return d;
+	}
+
+}
