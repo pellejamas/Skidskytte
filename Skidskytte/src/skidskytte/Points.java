a2940274915b43b6f867fd18177f62a98e90c769
Pelle Andersson <pellejamas@gmail.com>
2021-03-24 12:55:45 +0100

    Classen points har 2 strings som nu funkar.

--- Skidskytte/src/skidskytte/Points.java
+++ Skidskytte/src/skidskytte/Points.java
@@ -1,9 +1,9 @@
 package skidskytte;
-import java.util.Arrays;
+//import java.util.Arrays;
 public class Points {
 
-	private int[] pointsTab = { 60, 54, 48, 43, 40, 38, 36, 34, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21,
-								20, 19,	18, 17, 16, 15, 14, 13, 12, 11, 10, 9,  8,  7,  6,  5,  4,  3,  2,  1 };
+	private String[] pointsTab = { "60", "54", "48", "43", "40", "38", "36", "34", "32", "31", "30", "29", "28", "27", "26", "25", "24", "23", "22", "21",
+								"20", "19",	"18", "17", "16", "15", "14", "13", "12", "11", "10", "9",  "8",  "7",  "6",  "5",  "4",  "3",  "2",  "1" };
 	private String[] place = {  "1",  "2",  "3",  "4",  "5",  "6",  "7",  "8",  "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", 
 								"21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34",	"35", "36", "37", "38", "39", "40" };
 	// private int[] swap = new int[41];
@@ -11,13 +11,12 @@ public class Points {
 
 	public String PointsTabToPlace(String input) {
 		// Poäng till placering
-		String ints = Arrays.toString(pointsTab("\\[|\\]", "");
-				String str = "";
-		for (int i = 0; i < ints.length; i++) {
-			for (int j = 0; j < pointsTab.length; j++) {
-				if (pointsTab[j] == ints[i]) {
-					//strArray[i] = String.valueOf(pointsTab[i]);
-					str = str + place[j] + " ";					
+		String[] array = input.split(" ");
+		String str = "";
+		for (int i = 0; i < array.length; i++) {
+			for (int index = 0; index < pointsTab.length; index++) {
+				if (pointsTab[index].equals(array[i])) {
+					str = str + place[index] + " ";				
 				}
 			}
 		}
